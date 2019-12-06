66c745c09a764872ab9548df9c21ce18b9217631
Ben Bradshaw <benjamin.bradshaw@gmail.com>
2019-12-06 11:18:14 -0600

    day 5 & 6 (naive)

--- 2019/day05/solve.js
+++ 2019/day05/solve.js
@@ -1,61 +1,119 @@
-const input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,5,23,2,23,9,27,1,5,27,31,1,9,31,35,1,35,10,39,2,13,39,43,1,43,9,47,1,47,9,51,1,6,51,55,1,13,55,59,1,59,13,63,1,13,63,67,1,6,67,71,1,71,13,75,2,10,75,79,1,13,79,83,1,83,10,87,2,9,87,91,1,6,91,95,1,9,95,99,2,99,10,103,1,103,5,107,2,6,107,111,1,111,6,115,1,9,115,119,1,9,119,123,2,10,123,127,1,127,5,131,2,6,131,135,1,135,5,139,1,9,139,143,2,143,13,147,1,9,147,151,1,151,2,155,1,9,155,0,99,2,0,14,0";
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf-8');
 
 function parseInput(input){
-    let parsed = [];
-    for (let n of input.split(",")){
-        parsed.push(parseInt(n))
-    }
-    return parsed;
+    return input.split(",").map(x => Number(x));
 }
 
-function step(ip, program){
-    //console.log(`processing opcode ${program[ip]} at pos ${ip}.`);
-    switch(program[ip]){
-        case 1:
-            program[program[ip+3]] = program[program[ip+1]] + program[program[ip+2]];
-            break
-        case 2:
-            program[program[ip+3]] = program[program[ip+1]] * program[program[ip+2]];
-            break;
-        case 99:
-            return true;
-            break;
-        default:
-            throw new Error(`unknown opcode at position ${ip}`);
-    };
-    return false;
+class CPU{
+    constructor(program){
+        this.ip = 0;
+        this.should_run = true;
+        this.program = program;
+        this._input = [];
+    }
+
+    get input(){
+        return this._input.pop();
+    }
+
+    setInput(x){
+        this._input = x;
+    }
+
+    parameter(pos, mode){
+        const this_mode = mode[pos];
+        switch(this_mode){
+            case 0:
+                return this.program[this.program[this.ip + pos]];
+            case 1:
+                return this.program[this.ip + pos];
+            default:
+                throw new Error(`Unknown parameter mode ${this_mode}`);
+        }
+    }                   
+
+    decodeOpcode(){
+        const raw = this.program[this.ip].toString().padStart(5, '0');
+        const opcode = Number(raw.substr(3, 2));
+        const modes = {1 : Number(raw[2]), 2: Number(raw[1]), 3: Number(raw[0])}
+        return [opcode, modes];
+    }
+
+    output(o){
+        console.log(`   Output: ${o}`);
+    }
+
+    step(){
+        let [opcode, mode] = this.decodeOpcode();
+        //console.log(`processing opcode ${opcode} at pos ${this.ip}.`);
+        switch(opcode){
+            case 1:
+                this.program[this.program[this.ip+3]] = this.parameter(1, mode) + this.parameter(2, mode)
+                this.ip += 4;
+                break
+            case 2:
+                this.program[this.program[this.ip+3]] = this.parameter(1, mode) * this.parameter(2, mode)
+                this.ip += 4;
+                break;
+            case 3:
+                this.program[this.program[this.ip+1]] = this.input;
+                this.ip += 2;
+                break;
+            case 4:
+                this.output(this.parameter(1, mode))
+                this.ip += 2;
+                break;
+            case 5:
+                if (this.parameter(1, mode) !== 0)
+                    this.ip = this.parameter(2, mode);
+                else
+                    this.ip += 3;
+                break;
+            case 6:
+                if (this.parameter(1, mode) === 0)
+                    this.ip = this.parameter(2, mode);
+                else
+                    this.ip += 3;
+                break;
+            case 7:
+                if (this.parameter(1, mode) < this.parameter(2, mode))
+                    this.program[this.program[this.ip+3]] = 1;
+                else
+                    this.program[this.program[this.ip+3]] = 0;
+                this.ip += 4;
+                break;
+            case 8:
+                if (this.parameter(1, mode) === this.parameter(2, mode))
+                    this.program[this.program[this.ip+3]] = 1;
+                else
+                    this.program[this.program[this.ip+3]] = 0;
+                this.ip += 4;
+                break;
+            case 99:
+                this.should_run = false;
+                break;
+            default:
+                throw new Error(`unknown opcode ${opcode} at position ${this.ip}`);
+        };   
+    }
 }
 
-function solve(program, noun, verb){
-    let ip = 0;
-    program[1] = noun;
-    program[2] = verb;
-    while (true){
-        if (step(ip, program))
-            break;
-        ip += 4;
-    }
-    return program;
+function one(program){
+    let cpu = new CPU(program);
+    cpu.setInput([1]);
+    while (cpu.should_run){
+        cpu.step();
+    }
 }
 
-function one(input){
-    return solve(input, 12, 2)[0];
+function two(program){
+    let cpu = new CPU(program);
+    cpu.setInput([5]);
+    while (cpu.should_run){
+        cpu.step();
+    }
 }
-
-function two(input){
-    for (let noun = 0; noun < 100; noun++)
-        for (let verb = 0; verb < 100; verb++){
-            try{
-                if (solve([...input], noun, verb)[0] === 19690720)
-                    return verb + noun * 100;
-            }
-            catch(e){
-                continue;
-            }
-        }
-    console.log("solution not found!");
-}
-
 const program = parseInput(input);
 console.log(one([...program]));
 console.log(two([...program]));
---
+++ 2019/day06/solve.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf8').split('\n');
+/*const input = `COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`.split("\n");*/
+
+class SpaceObject{
+    constructor(name, orbits){
+        this.name = name;
+        this.orbits = orbits;
+    }
+
+    static fromLine(txt){
+        let [o,n] = txt.split(")");
+        return new SpaceObject(n, o);
+    }
+}
+
+class leafFinder{
+    constructor(tree){
+        this.tree = tree;
+        this.map = new Map();
+    }
+
+    get(name){
+        if (!this.map.has(name))
+            for (let o of this.tree)
+                if (o.name === name)
+                    this.map[name] = o;
+        return this.map[name];
+    }
+}
+
+function createTree(problem){
+    let tree = [new SpaceObject("COM", null)];
+    for (let line of problem){
+        tree.push(SpaceObject.fromLine(line));
+    }
+    return tree;
+}
+
+function getParents(lookup, start){
+    let cur = lookup.get(start);
+    let path = [];
+    while (cur.orbits !== null){
+        cur = lookup.get(cur.orbits);
+        path.push(cur);
+    }
+    return path;
+}
+
+function one(problem){
+    let tree = createTree(problem);
+    let lookup = new leafFinder(tree);
+    let sum = 0;
+    for (let obj of tree){
+        while (obj.orbits != null){
+            sum += 1;
+            obj = lookup.get(obj.orbits);
+        }
+    }
+    return sum;
+}
+
+function two(problem){
+    let lookup = new leafFinder(createTree(problem));
+    const santa_path = getParents(lookup, "SAN").map(x => x.name);
+    const you_path = getParents(lookup, "YOU").map(x => x.name);
+    const common_names = santa_path.filter(x => you_path.includes(x));
+    let total_distance = 0;
+    let cur = lookup.get("YOU");
+    while (!common_names.includes(cur.orbits)){
+        console.log(cur.name);
+        total_distance += 1;
+        cur = lookup.get(cur.orbits);
+        //console.log(`distance ${total_distance} moving to ${cur.name}`);
+    }
+    cur = lookup.get("SAN");
+    while (!common_names.includes(cur.orbits)){
+        console.log(cur.name);
+        total_distance += 1;
+        cur = lookup.get(cur.orbits);
+        //console.log(`distance ${total_distance} moving to ${cur.name}`);
+    }
+    return total_distance;
+}
+
+//console.log(one(input));
+console.log(two(input));
