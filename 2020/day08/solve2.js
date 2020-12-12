575fdaab4dccaba8ed2e815ec1d0cb1e81c3f34f
Ben Bradshaw <benjamin.bradshaw@gmail.com>
2020-12-12 00:49:54 -0600

    unclobber d8

--- 2020/day08/solve2.js
+++ 2020/day08/solve2.js
@@ -1,30 +1,68 @@
-function union(setA, setB) {
-    let _union = new Set(setA);
-    for (let elem of setB) {
-        _union.add(elem);
-    }
-    return _union;
-}
-
-function solve1(input, step){
-    let groups = [];
-    let persons = [];
-    for (const line of input.split("\n")){
-        if (line.length === 0){
-            groups.push(persons);
-            persons = [];
-        } else {
-            persons.push(new Set(line.split('')));
-			gauge(`parsing ${Array.from(persons)}`);
-			await step();
+async function run(instructions, step) {
+	let acc = 0;
+	let seen = new Set();
+	let ip = 0;
+	let last_ip = 0;
+	while (true) {
+		code = instructions[ip];
+		seen.add(ip);
+		last_ip = ip;
+		switch (code.instruction) {
+			case 'acc':
+				acc += code.value;
+				ip++;
+				break;
+			case 'jmp':
+				ip += code.value;
+				break;
+			case 'nop':
+				ip++;
+				break;
+			case 'end':
+				log(`program end reached! at ${ip}`);
+				return [acc, seen];
+		}
+		if (seen.has(ip)) {
+			log(`Loop detected from '${last_ip}: ${instructions[last_ip].instruction} ${instructions[last_ip].value}'` +
+				` -> '${ip}: ${code.instruction} ${code.value}'`);
+			return [null, seen];
 		}
 	}
-    groups.push(persons);
-    let answer = 0;
-    for (const group of groups){
-        if (!group.length) continue;
-        let yeses = group.reduce((acc, p) => union(acc,p));
-        answer += yeses.size;
-    }
-   showAnswer(answer);
+}
+
+async function solve2(input, step) {
+	let instructions = [];
+	let lines = input.split("\n");
+	let i = 0;
+	for (i=0; i<lines.length; i++) {
+		let [instruction, val] = lines[i].split(" ");
+		instructions.push({ instruction, value: parseInt(val)});
+	}
+	log(`parsed ${i} instructions.`);
+	let [_, usedPath] = await run(instructions, step);
+	log(`pristine run path of ${usedPath.size} instructions`);
+	instructions.push({ instruction: 'end', value: 0});
+
+	for (i=0; i<instructions.length; i++) {
+		const code = instructions[i];
+		if (code.instruction === 'acc' || code.instruction === 'end') continue;
+		gauge(`toggling instruction of '${JSON.stringify(code)}'`);
+		await step();
+		if (!usedPath.has(i)) continue;
+		let new_code;
+		if (code.instruction === 'nop') {
+			new_code = Object.assign({}, code, { instruction: 'jmp' });
+			log(`replacing nop with jmp at ${i}`);
+		}
+		else {
+			new_code = Object.assign({}, code, { instruction: 'nop' });
+			log(`replacing jmp with nop at ${i}`);
+		}
+		const result = await run([...instructions.slice(0, i), new_code, ...instructions.slice(i + 1)], step);
+
+		if (result[0] !== null) {
+			showAnswer(result[0]);
+			break;
+		}
+	}
 }
