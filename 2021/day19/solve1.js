cce631eecb8fab7e5ee1adadb79ac3557908918c
Ben Bradshaw <benjamin.bradshaw@gmail.com>
2022-01-03 14:26:20 -0600

    closer

--- 2021/day19/solve1.js
+++ 2021/day19/solve1.js
@@ -1,153 +1,270 @@
-const JS = JSON.stringify;
-
-class BeaconMatcher {
-	constructor() {
-		this.transformationMatrices = new Map();
-		this.beacons = new Map();
-		this.deltas = new Map();
-	}
-
-	setTransformMatrix(scanner_id, matrix) {
-		this.transformationMatrices.set(scanner_id, matrix);
-	}
-
-	add_beacons(scanner_id, beacons) {
-		const scanner_matrix = this.transformationMatrices.get(scanner_id);
-
-		let i = this.beacons.size;
-		for (let beacon of beacons) {
-			for (const knownBeaconIdx of Array.from(this.beacons.keys())) {
-				const knownBeacon = this.beacons.get(knownBeaconIdx);
-				beacon = beacon.transform(scanner_matrix);
-				this.deltas.set(beacon.sub(knownBeacon).s, [i, knownBeaconIdx]);
-				i++;
-				this.beacons.set(i, beacon);
-			}
-		}
-	}
-
-	find_matching_delta(delta) {
-		return this.deltas.get(delta.s);
-	}
-}
-
 class Vec3D {
 	constructor(x, y, z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+		Object.assign(this, { x, y, z });
 	}
 
-	transform(tm) {
+	transformBy(rotateMatrix) {
 		let x, y, z;
-		x = this.x * tm[0][0] + this.y * tm[0][1] + this.z * tm[0][2] + tm[0][3];
-		y = this.x * tm[1][0] + this.y * tm[1][1] + this.z * tm[1][2] + tm[1][3];
-		z = this.z * tm[2][0] + this.y * tm[2][1] + this.z * tm[2][2] + tm[2][3];
+		x = this.x * rotateMatrix[0][0] + this.y * rotateMatrix[0][1] + this.z * rotateMatrix[0][2];
+		y = this.x * rotateMatrix[1][0] + this.y * rotateMatrix[1][1] + this.z * rotateMatrix[1][2];
+		z = this.z * rotateMatrix[2][0] + this.y * rotateMatrix[2][1] + this.z * rotateMatrix[2][2];
 		return new Vec3D(x, y, z);
 	}
 
-	static identityTransform() {
-		return [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]];
-	}
-
-	static allRotations() {
-		let rots = [];
-		for (let i = -2; i < 3; i++)
-			for (let j = -2; j < 3; j++)
-				for (let k = -2; k < 3; k++) {
-					if (Math.abs(i) === Math.abs(j) || Math.abs(i) === Math.abs(k) || Math.abs(j) == Math.abs(k)) continue;
-					let blank = [...Array(3)].map(a => Array(4).fill(0));
-					blank[0][Math.abs(i)] = i === 0 ? 1 : i / Math.abs(i);
-					blank[1][Math.abs(j)] = j === 0 ? 1 : j / Math.abs(j);
-					blank[2][Math.abs(k)] = k === 0 ? 1 : k / Math.abs(k);
-					rots.push(blank);
-				}
-		return rots;
-	}
-
-	static findInverse(m, candidates) {
-		let i = Vec3D.identityTransform();
-		i.push([0,0,0,1]);
-		for (let c of candidates) {
-			if (JS(multiplyMatrices(m, c)) === JS(i))
-				return c;
-		}
-	}
-
-	get s() {
-		return `${this.x},${this.y},${this.z}`;
-	}
-
 	sub(other) {
-		return new Vec3D(this.x - other.x, this.y - other.y, this.z - other.z);
+		let x, y, z;
+		x = this.x - other.x;
+		y = this.y - other.y;
+		z = this.z - other.z;
+		return new Vec3D(x, y, z);
+	}
+
+	add(other) {
+		return new Vec3D(this.x + other.x, this.y + other.y, this.z + other.z);
+	}
+
+	equals(other) {
+		return this.x === other.x && this.y === other.y && this.z === other.z;
+	}
+
+	toString() {
+		return `(${this.x},${this.y},${this.z})`;
+	}
+
+	static parse(s) {
+		let m = /\(([^,]+),([^,]+),([^,]+)\)/.exec(s);
+		return new Vec3D(parseInt(m[1]), parseInt(m[2]), parseInt(m[3]));
 	}
 }
 
-function multiplyMatrices(m1, m2) {
-	var result = [];
-	for (var i = 0; i < m1.length; i++) {
-		result[i] = [];
-		for (var j = 0; j < m2[0].length; j++) {
-			var sum = 0;
-			for (var k = 0; k < m1[0].length; k++) {
-				sum += m1[i][k] * m2[k][j];
+function getAllRotations() {
+	return [/*[[1, 0, 0], [0, 1, 0], [0, 0, 1]],*/
+	[[1, 0, 0], [0, 0, 1], [0, -1, 0]],
+	[[1, 0, 0], [0, -1, 0], [0, 0, -1]],
+	[[1, 0, 0], [0, 0, -1], [0, 1, 0]],
+	[[0, 0, -1], [0, 1, 0], [1, 0, 0]],
+	[[0, 0, -1], [1, 0, 0], [0, -1, 0]],
+	[[0, 0, -1], [0, -1, 0], [-1, 0, 0]],
+	[[0, 0, -1], [-1, 0, 0], [0, 1, 0]],
+	[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
+	[[-1, 0, 0], [0, 0, -1], [0, -1, 0]],
+	[[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
+	[[-1, 0, 0], [0, 0, 1], [0, 1, 0]],
+	[[0, 0, 1], [0, 1, 0], [-1, 0, 0]],
+	[[0, 0, 1], [-1, 0, 0], [0, -1, 0]],
+	[[0, 0, 1], [0, -1, 0], [1, 0, 0]],
+	[[0, 0, 1], [1, 0, 0], [0, 1, 0]],
+	[[0, 1, 0], [-1, 0, 0], [0, 0, 1]],
+	[[0, 1, 0], [0, 0, 1], [1, 0, 0]],
+	[[0, 1, 0], [1, 0, 0], [0, 0, -1]],
+	[[0, 1, 0], [0, 0, -1], [-1, 0, 0]],
+	[[0, -1, 0], [1, 0, 0], [0, 0, 1]],
+	[[0, -1, 0], [0, 0, 1], [-1, 0, 0]],
+	[[0, -1, 0], [-1, 0, 0], [0, 0, -1]],
+	[[0, -1, 0], [0, 0, -1], [1, 0, 0]]];
+}
+
+function diffsig(vec1, vec2) {
+	return JSON.stringify([Math.abs(vec1.x - vec2.x), Math.abs(vec1.y - vec2.y), Math.abs(vec1.z - vec2.z)].sort((a, b) => a - b));
+}
+
+
+class KnownBeacons {
+
+	constructor(scanners) {
+		this.scanners = scanners;
+
+		this.deltas = new Map();
+		this.known_beacons = new Map();
+	}
+
+	add_known(scanner_idx, beacon_idx, abs_beacon) {
+		if (!this.known_beacons.has(abs_beacon.toString()))
+			this.known_beacons.set(abs_beacon.toString(), [[scanner_idx, beacon_idx]]);
+		else
+			this.known_beacons.get(abs_beacon.toString()).push([scanner_idx, beacon_idx]);
+	}
+
+	find_diff(diff) {
+		let results = [];
+		for (const [k1, entries1] of this.known_beacons.entries()) {
+			let beacon1 = this.scanners[entries1[0][0]][entries1[0][1]];
+			for (const [k2, entries2] of this.known_beacons.entries()) {
+				if (k1 === k2) continue;
+				let beacon2 = this.scanners[entries2[0][0]][entries2[0][1]];
+				let s = `${beacon1}->${beacon2}`;
+				if (this.deltas.has(s)) {
+					if (diff === this.deltas.get(s))
+						results.push([beacon1, beacon2]);
+				}
+				else {
+					const newdiff = diffsig(beacon1, beacon2);
+					this.deltas.set(s, newdiff);
+					if (diff === newdiff)
+						results.push([beacon1, beacon2]);
+				}
 			}
-			result[i][j] = sum;
 		}
+		return results;
 	}
-	return result;
 }
 
-function findInverseTransforms() {
-	let rots = Vec3D.allRotations();
-	rots.forEach(m => m.push([0,0,0,1]));
-	let inv = {};
-	for (let r of rots) {
-		let i = Vec3D.findInverse(r, rots);
-		if (!i)
-			throw new Error(`cannot find inverse for rot matrix ${JS(r)}`);
-		inv[r] = i;
-	}
-	return i;
+function matching_deltas([a, b], [c, d]) {
+	for (const dim of ['x', 'y', 'z'])
+		if (a[dim] - b[dim] !== c[dim] - d[dim])
+			return false;
+	return true;
 }
 
 async function solve1(input, step) {
-	let scanner = new Map();
-	let matcher = new BeaconMatcher();
+	log('running unit tests');
+	test();
+	const allRotations = getAllRotations();
+	let scanner = {};
 	let scanner_i;
+	let unknown_scanners = new Set();
+
 	for (const line of input.split("\n")) {
 		if (line === "") continue;
 		let m = /--- scanner (\d+) ---/.exec(line);
 		if (m !== null) {
 			scanner_i = parseInt(m[1]);
-			scanner.set(scanner_i, []);
+			scanner[scanner_i] = [];
+			unknown_scanners.add(scanner_i);
 			continue;
 		}
 		let coords = line.split(",").map(i => parseInt(i));
-		scanner.get(scanner_i).push(new Vec3D(...coords));
+		scanner[scanner_i].push(new Vec3D(...coords));
 	}
 
-	matcher.setTransformMatrix(0, Vec3D.identityTransform());
-	let inverses = findInverseTransforms();
+	let known_beacons = new KnownBeacons(scanner);
+	log(`finished parsing`);
 
-	let beacons = scanner.get(0);
-	matcher.add_beacons(0, beacons);
+	for (let i = 0; i < scanner[0].length; i++)
+		known_beacons.add_known(0, i, scanner[0][i]);
 
-	let beacons2 = scanner.get(1);
-	rotationLoop:
-	for (let rotationMatrix of Vec3D.allRotations()) {
-		let hits = 0;
-		for (let i = 0; i < beacons2.length - 1; i++) {
-			for (let j = i + 1; j < beacons2.length; j++) {
-				let match = matcher.find_matching_delta(beacons[i].sub(beacons[j]).transform(rotationMatrix));
-				if (match != undefined) {
-					hits++;
-					log(`${JS(beacons[i])} -> ${JS(beacons[j])} is the same as ${JS(match[0])} -> ${JS(match[1])}`);
+	let lastSize = unknown_scanners.size;
+	unknown_scanners.delete(0);
+	log(`set scanner 0 to solved.`);
+
+	while (unknown_scanners.size > 0) {
+		if (unknown_scanners.size === lastSize) {
+			showAnswer("failure :(");
+			break;
+		}
+		lastSize = unknown_scanners.size;
+		for (const uk_scanner_id of unknown_scanners) {
+			await step(null);
+			let matching_pts = {};
+			let matched_scanners = {};
+
+			for (let i = 0; i < scanner[uk_scanner_id].length - 1; i++) {
+				const uk_beacon1 = scanner[uk_scanner_id][i];
+				for (let j = i + 1; j < scanner[uk_scanner_id].length; j++) {
+					const uk_beacon2 = scanner[uk_scanner_id][j];
+					const diff = diffsig(uk_beacon1, uk_beacon2);
+					for (const [m_beacon1, m_beacon2] of known_beacons.find_diff(diff)) {
+						const finding = { known: [m_beacon1, m_beacon2], unknown: [uk_beacon1, uk_beacon2] };
+						if (matching_pts.hasOwnProperty(uk_scanner_id))
+							matching_pts[uk_scanner_id].push(finding);
+						else
+							matching_pts[uk_scanner_id] = [finding];
+
+						if (!matched_scanners.hasOwnProperty(uk_scanner_id))
+							matched_scanners[uk_scanner_id] = new Set();
+						matched_scanners[uk_scanner_id].add(uk_beacon1.toString());
+						matched_scanners[uk_scanner_id].add(uk_beacon2.toString());
+					}
 				}
 			}
+
+			for (const [matched_scanner, uniq_matched] of Object.entries(matched_scanners)) {
+				if (uniq_matched.size >= 12) {
+					log(`found ${uniq_matched.size} new matches in scanner ${matched_scanner}.`)
+					let rotation, translation;
+
+					rotloop:
+					for (rotation of allRotations) {
+						let matched = {};
+						let hadMatched = false;
+						for (const { known, unknown } of matching_pts[matched_scanner]) {
+							let rotated = unknown.map(beacon => beacon.transformBy(rotation));
+
+							if (matching_deltas(known, rotated)) {
+								if (!hadMatched) log(`matched ${known[0]} to ${unknown[0]} and ${known[1]} to ${unknown[1]} with rotation ${allRotations.indexOf(rotation)}`);
+								hadMatched = true;
+								matched[known[0].toString()] = { known: known[0], candidate: rotated[0] };
+								matched[known[1].toString()] = { known: known[1], candidate: rotated[1] };
+							}
+							else if (matching_deltas(known, [rotated[1], rotated[0]])) {
+								if (!hadMatched) log(`rev-matched ${known[1]} to ${unknown[0]} and ${known[0]} to ${unknown[1]} with rotation ${allRotations.indexOf(rotation)}`);
+								hadMatched = true;
+								matched[known[1].toString()] = { known: known[1], candidate: rotated[0] };
+								matched[known[0].toString()] = { known: known[0], candidate: rotated[1] };
+							}
+							else if (hadMatched) {
+								log(`rotation had matched for ${matched_scanner} but failed on a beacon`);
+								continue rotloop;
+							}
+							else{
+								continue rotloop;
+							}
+						}
+						for (const { known, candidate } of Object.values(matched)) {
+							if (!translation) {
+								translation = known.sub(candidate);
+								continue;
+							}
+							if (!translation.add(candidate).equals(known)) {
+								log(`translation failed ${candidate} + ${translation} does not equal ${known}`);
+								continue rotloop;
+							}
+						}
+
+						log(`fully matched scanner ${matched_scanner} at position ${translation}`);
+						for (let i = 0; i < scanner[matched_scanner].length; i++)
+							known_beacons.add_known(matched_scanner, i, scanner[matched_scanner][i].transformBy(rotation).add(translation));
+						unknown_scanners.delete(matched_scanner);
+						break rotloop;
+					}
+				}
+
+			}
+
 		}
+
 	}
-	showAnswer("lol");
 
+}
 
+function test() {
+	let a = new Vec3D(1, 2, 3);
+	let b = new Vec3D(4, 5, 6);
+	let c = new Vec3D(10, 11, 15);
+	let scanners = { 0: [a, b, c] };
+	assert(a.sub(b).add(b).equals(a), "basic transitive vec test");
+	let beacons = new KnownBeacons(scanners);
+	for (let i = 0; i < scanners[0].length; i++) {
+		beacons.add_known(0, i, scanners[0][i]);
+	}
+	assert(diffsig(a, b) !== diffsig(a, c), "diff sigs are unique");
+	let rotation = getAllRotations()[1];
+
+	let a2 = a.transformBy(rotation);
+
+	assert(!a.equals(a2), "rotation matrix transforms");
+
+	let b2 = b.transformBy(rotation);
+
+	assert(diffsig(a, b) === diffsig(a2, b2), "diff sigs are rotation invariant");
+
+	let c2 = c.transformBy(rotation);
+	let d2 = new Vec3D(0, 1, 2);
+
+	assert(diffsig(a, b) !== diffsig(a2, d2), "diff sigs are hopefully unique");
+
+	let matching = beacons.find_diff(diffsig(a, b));
+	assert(matching.length === 2, "should be 2 matches for each dir");
+	assert(matching[0][0].equals(a) || matching[0][1].equals(a), "vec a did not match!")
+	assert(matching[0][0].equals(b) || matching[0][1].equals(b), "vec b did not match!")
 }
